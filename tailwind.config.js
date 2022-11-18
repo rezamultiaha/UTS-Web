35e826ca0561aa1e81fcce73fc3f4712620267e9
rezamultiaha <108449219+rezamultiaha@users.noreply.github.com>
2022-11-18 17:14:32 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5E8B7E',
+        secondary: '#ff3b07',
+        textSecondary: '#969696',
+        textThree: '#5E8B7E',
+        header: '#F6F9FC',
+        footer: '#0E2123',
+        fontColor: '#636363',
+        three: '#F7FBFA',
+      },
+    },
+  },
+};
